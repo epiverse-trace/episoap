b0ed59b052261dd24b5d98a13dc3d10945e76bf9
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-10-13 17:34:59 +0200

    Create basic app to display report

---
+++ automated_pipelines/app.R
@@ -0,0 +1,31 @@
+library(shinydashboard)
+
+shinyApp(
+    ui = dashboardPage(
+        dashboardHeader(title = "Automated reports for transmissibility estimation"),
+        dashboardSidebar(
+            fileInput("inputData", "Data file (ignored for now)"),
+            radioButtons("transmissibilityPkg", "Reproduction number estimation", c("EpiEstim", "EpiNow2", "R0")),
+            radioButtons("contactdataPkg", "Contact data", c("conmat", "contactdata", "socialmixr"))
+            #                radioButtons("outputFormat", "Output format", c("HTML", "PDF"), inline = TRUE),
+            #                downloadButton("report", "Download report")
+        ),
+        dashboardBody(
+            htmlOutput("report")
+        )
+    ),
+    server = function(input, output) {
+        output$report <- renderUI({
+            params <- list(
+                epicurve_unit = "week",
+                incomplete_days = 7L,
+                r_estim_window = 21L,
+                si_mean = 4.2,
+                si_sd = 4.9
+            )
+            includeHTML(
+                rmarkdown::render("../reports/transmissibility.Rmd")
+            )
+        })
+    }
+)
