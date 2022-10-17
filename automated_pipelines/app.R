dad4be85bfcb7c8bd2c808f1f8d7568959129817
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-10-17 11:56:44 +0200

    Expose more settings to user

--- automated_pipelines/app.R
+++ automated_pipelines/app.R
@@ -1,11 +1,18 @@
 library(shinydashboard)
+library(shinyWidgets)
 
 shinyApp(
     ui = dashboardPage(
         dashboardHeader(title = "Automated reports"),
         dashboardSidebar(
             fileInput("inputData", "Data file (ignored for now)"),
-            radioButtons("transmissibilityPkg", "Reproduction number estimation", c("EpiEstim", "EpiNow2", "R0")),
+            h4("Epicurves"),
+            sliderTextInput("epicurve_unit", "Epicurve Unit", choices = "week"),
+            sliderInput("incomplete_days", "Incomplete days", min = 0L, max = 21L, value = 7L, ticks = FALSE, step = 1L, post = " days"),
+            h4("Reproduction number estimation"),
+            radioButtons("transmissibilityPkg", "R package" , c("EpiEstim", "EpiNow2", "R0")),
+            sliderInput("r_estim_window", "Estimation window", min = 7L, max = 35L, value = 21L, ticks = FALSE, step = 1L, post = " days"),
+            h4("Final size estimation"),
             radioButtons("contactdataPkg", "Contact data", c("conmat", "contactdata", "socialmixr"))
             #                radioButtons("outputFormat", "Output format", c("HTML", "PDF"), inline = TRUE),
             #                downloadButton("report", "Download report")
