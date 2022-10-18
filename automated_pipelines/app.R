ed9be65267bd5bd70db948fb95376f9f97d69837
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-10-18 11:30:27 +0200

    Add shiny front-end (#30)

---
+++ automated_pipelines/app.R
@@ -0,0 +1,38 @@
+library(shinydashboard)
+library(shinyWidgets)
+
+shinyApp(
+    ui = dashboardPage(
+        dashboardHeader(title = "Automated reports"),
+        dashboardSidebar(
+            fileInput("inputData", "Data file (ignored for now)"),
+            h4("Epicurves"),
+            sliderTextInput("epicurve_unit", "Epicurve Unit", choices = "week"),
+            sliderInput("incomplete_days", "Incomplete days", min = 0L, max = 21L, value = 7L, ticks = FALSE, step = 1L, post = " days"),
+            h4("Reproduction number estimation"),
+            radioButtons("transmissibilityPkg", "R package" , c("EpiEstim", "EpiNow2", "i2extras", "R0")),
+            sliderInput("r_estim_window", "Estimation window", min = 7L, max = 35L, value = 21L, ticks = FALSE, step = 1L, post = " days"),
+            h4("Final size estimation"),
+            radioButtons("contactdataPkg", "Contact data", c("conmat", "contactdata", "socialmixr"))
+        ),
+        dashboardBody(
+            shinycssloaders::withSpinner(
+              htmlOutput("report")
+            )
+        )
+    ),
+    server = function(input, output) {
+        output$report <- renderUI({
+            includeHTML(
+                rmarkdown::render(
+                    "../reports/transmissibility.Rmd",
+                    params = list(
+                        epicurve_unit = input$epicurve_unit,
+                        incomplete_days = input$incomplete_days,
+                        r_estim_window = input$r_estim_window
+                    )
+                )
+            )
+        })
+    }
+)
