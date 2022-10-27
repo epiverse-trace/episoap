4bf62ef6d3aa6e43cbf12441f31494db2af257f8
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-10-27 17:02:56 +0200

    Run styler

--- tests/testthat/test-render_templates.R
+++ tests/testthat/test-render_templates.R
@@ -1,21 +1,18 @@
 test_that("all templates render (at least with default params)", {
-
   skip_on_cran()
   skip_on_ci()
 
   for (tp in list_templates()) {
-
     expect_no_error(
       suppressMessages(suppressWarnings(
         rmarkdown::render(
           system.file("rmarkdown", "templates", tp, "skeleton", "skeleton.Rmd",
-                      package = "soap"),
+            package = "soap"
+          ),
           output_file = tempfile(),
           quiet = TRUE
         )
       ))
     )
-
   }
-
 })
