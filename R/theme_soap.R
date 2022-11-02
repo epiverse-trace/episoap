efe08db457312179630b7fe2c562d1bb50397f9b
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-11-02 15:43:23 +0100

    Fix lintr warnings

--- R/theme_soap.R
+++ R/theme_soap.R
@@ -1,17 +1,23 @@
-#' @importFrom ggplot2 %+replace% theme theme_grey element_rect element_text element_line
+#' @importFrom ggplot2 %+replace%
+#' @importFrom ggplot2 theme theme_grey element_rect element_text element_line
 theme_custom <- function(...) {
-  # https://ggplot2.tidyverse.org/articles/ggplot2-in-packages.html#creating-a-new-theme
+
+  # https://ggplot2.tidyverse.org/articles/ggplot2-in-packages.html
 
   custom_grey <- "#505B5B"
   green_grey <- "#5E7E80"
 
-  theme_custom <- theme_grey(...) %+replace%
+  theme_grey(...) %+replace%
     theme(
       plot.background = element_rect(fill = "white"),
       panel.grid.major = element_line(colour = "#96a3a3"),
       panel.grid.minor = element_line(colour = "#9DC1C2", linetype = "dotted"),
       panel.background = element_rect(colour = custom_grey, fill = "white"),
-      strip.background = element_rect(fill = green_grey, color = "white", size = 1),
+      strip.background = element_rect(
+        fill = green_grey,
+        color = "white",
+        size = 1
+      ),
       strip.text = element_text(colour = "white"),
       text = element_text(size = 16, colour = custom_grey),
       axis.ticks = element_line(colour = custom_grey)
