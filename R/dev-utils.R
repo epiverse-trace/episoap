a6017748c74661bbed3c6215f512540a8895656c
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-05-29 10:10:32 +0200

    Add release item reminding to bump episoap versio number Follow up of #92

--- R/dev-utils.R
+++ R/dev-utils.R
@@ -5,6 +5,7 @@ release_bullets <- function() {
   c(
     "Run `goodpractice::gp()`",
     "Update lockfiles embedded in templates",
+    "Bump episoap version number is lockfiles",
     "Test templates with renv::use(isolate = TRUE)"
   )
 
